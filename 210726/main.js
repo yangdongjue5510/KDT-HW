44f6991c13917fe9d137d5fe756a5042ae333455
yangdongjue5510 <djwhy5510@gmail.com>
2021-08-10 19:37:47 +0900

    fix: 210806 code fix

--- 210726/main.js
+++ 210726/main.js
@@ -14,7 +14,7 @@ const data = [
 
 // 사용자 입력 받기
 const line = prompt('최대 금액을 입력해주세요.');
-const amount = +line;
+const amount = Number(line);
 
 // 주어진 금액으로 살 수 있는 가장 비싼 상품을 구함
 const item = getItemByAmount(data, amount);
@@ -29,13 +29,13 @@ alert(msg);
 // 아래에 getItemByAmount 함수를 작성하세요.
 
 function getItemByAmount(data, amount){
-    let tmpProduct = { name: '', price: 0 } //임시적으로 값을 저장할 객체 선언
-    if (amount<500) //가장 저렴한 제품보다 적은 돈을 입력시 null반환
+    let tmpProduct = { name: '', price: 0 } //임시적으로 값을 저장할 객체 선언 filter로 한줄로 만들기.
+    if (amount < 500) //가장 저렴한 제품보다 적은 돈을 입력시 null반환
         return null
-    for(let product of data){   //data 배열의 요소에 접근
-        if(amount>=product.price){  //가진 돈으로 살 수 있는 제품이면
-            if(product.price>tmpProduct.price){ //이전에 임시로 저장한 제품보다 비싼 제품이면
-                tmpProduct  = product //해당 제품을 임시로 저장.
+    for (let product of data){   //data 배열의 요소에 접근
+        if (amount >= product.price){  //가진 돈으로 살 수 있는 제품이면 tmpProduct.price는 어차피 amount보다 작음!!
+            if (product.price > tmpProduct.price){ //이전에 임시로 저장한 제품보다 비싼 제품이면
+                tmpProduct = product //해당 제품을 임시로 저장.
             }
         }
     }
