07fc25757eaebae04bab53892f776dcddb3881b2
yangdongjue5510 <djwhy5510@gmail.com>
2021-08-20 17:15:39 +0900

    fix: hw4 fixed.

--- 210810/src/assets/js/main.js
+++ 210810/src/assets/js/main.js
@@ -1,7 +1,11 @@
 // 글쓰기 버튼 페이지 이동 방지
 const $submit = document.querySelector('button[type="submit"]');
-$submit &&
+
+document.addEventListener("DOMContentLoaded", function(){
+    alert("DOM ready!");
+    $submit &&
     $submit.addEventListener("click", e => {
         e.preventDefault();
         alert("Great!");
     });
+});
